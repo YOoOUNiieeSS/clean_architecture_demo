c917adbef41be33019b42a228407695432b5e797
Mohamed Younies <mohamed.yonis@candsmena.com>
2022-11-09 15:41:33 +0200

    api consumer

--- lib/features/random_qoute/data/data_sources/random_quote_remote_data_source.dart
+++ lib/features/random_qoute/data/data_sources/random_quote_remote_data_source.dart
@@ -1,26 +1,19 @@
-import 'dart:convert';
-
+import 'package:clean_architecture/core/api/api_consumer.dart';
 import 'package:clean_architecture/core/api/end_points.dart';
-import 'package:clean_architecture/core/error/exceptions.dart';
-import 'package:clean_architecture/core/utils/app_strings.dart';
 import 'package:clean_architecture/features/random_qoute/data/models/quote_model.dart';
-import 'package:http/http.dart' as http;
 
 abstract class RandomQuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
 }
 
 class RandomQuoteRemoteDataSourceImpl implements RandomQuoteRemoteDataSource {
-  http.Client client;
-  RandomQuoteRemoteDataSourceImpl({required this.client});
+  ApiConsumer apiConsumer;
+  RandomQuoteRemoteDataSourceImpl({required this.apiConsumer});
   @override
   Future<QuoteModel> getRandomQuote() async {
-    final res = await client.get(Uri.parse(EndPoints.randomQuote),
-        headers: {AppStrings.contentType:AppStrings.applicationJson});
-    if (res.statusCode == 200) {
-      return QuoteModel.fromJson(json.decode(res.body));
-    } else {
-      throw ServerException();
-    }
+    final res = await apiConsumer.get(
+      path: EndPoints.randomQuote,
+    );
+    return QuoteModel.fromJson(res);
   }
 }
